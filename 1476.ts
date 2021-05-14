7e662a8ec0b0cfa5eabeb82cd943cc95edd797ab
Arnab Rahman <arnabrahman@hotmail.com>
2021-05-14 11:34:20 +0600

    Subrectangle Queries

---
+++ 1476.ts
@@ -0,0 +1,32 @@
+class SubrectangleQueries {
+  private rectange: number[][];
+
+  constructor(rectangle: number[][]) {
+    this.rectange = rectangle;
+  }
+
+  updateSubrectangle(
+    row1: number,
+    col1: number,
+    row2: number,
+    col2: number,
+    newValue: number
+  ): void {
+    for (let row = row1; row <= row2; row++) {
+      for (let col = col1; col <= col2; col++) {
+        this.rectange[row][col] = newValue;
+      }
+    }
+  }
+
+  getValue(row: number, col: number): number {
+    return this.rectange[row][col];
+  }
+}
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * var obj = new SubrectangleQueries(rectangle)
+ * obj.updateSubrectangle(row1,col1,row2,col2,newValue)
+ * var param_2 = obj.getValue(row,col)
+ */
