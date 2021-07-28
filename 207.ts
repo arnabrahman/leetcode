4df83584b0fc70733b901361f93f52aa5941c411
Arnab Rahman <arnabrahman@hotmail.com>
2021-07-28 08:48:41 +0600

    207 ongoing

---
+++ 207.ts
@@ -0,0 +1,10 @@
+function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+    let courses = new Array(numCourses)
+
+    for (let index = 0; index < prerequisites.length; index++) {
+        const element = prerequisites[index];
+
+    }
+};
+
+console.log(canFinish(2, [[1, 0], [0, 1]]))
