2083e4a29b1934ac238a74d7a0b2546f91543068
arnabrahman <arnabrahman@hotmail.com>
2022-02-27 09:46:41 +0600

    876 Middle of the Linked List O(n/2) time

--- 876.ts
+++ 876.ts
@@ -2,10 +2,14 @@ function middleNode(head: ListNode | null): ListNode | null {
     if (head.next == null) return head
     if (head.next.next == null) return head.next
     let cursor = head;
-    let secondCursor = head.next?.next
+    let secondCursor = head.next.next
+    let count = 3;
     while (secondCursor) {
         cursor = cursor.next
+        if (secondCursor.next) count++
+        if (secondCursor.next?.next) count++
         secondCursor = secondCursor.next?.next
     }
+    if (count % 2 == 0) return cursor.next
     return cursor
 };
