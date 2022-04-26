93ed17300db04d462698c6eabf368bba1af28ba1
arnabrahman <arnabrahman@hotmail.com>
2022-04-26 09:46:01 +0600

    Two sum in O(n) times

--- 1.ts
+++ 1.ts
@@ -1,19 +1,17 @@
 function twoSum(nums: number[], target: number): number[] {
-    let sortedArray = [...nums].sort((a, b) => a - b)
-    let length = sortedArray.length
-    let left = 0;
-    let right = length - 1
-    while (length > 0) {
-        if (sortedArray[left] + sortedArray[right] == target) break
-        sortedArray[left] + sortedArray[right] > target ? right-- : left++
-        length--
-    }
-    let results = []
+    const result: number[] = []
+    const hashMap = new Map<number, number>()
     for (let i = 0; i < nums.length; i++) {
-        if (sortedArray[left] == nums[i]) results.push(i)
-        else if (sortedArray[right] == nums[i]) results.push(i)
+        const diff = target - nums[i]
+        if (hashMap.has(diff)) {
+            result.push(hashMap.get(diff), i)
+            break
+        }
+        else {
+            hashMap.set(nums[i], i)
+        }
     }
-    return results
+    return result
 };
 
-console.log(twoSum([2, 7, 4, 3, 11, 8], 9))
+console.log(twoSum([2, 7, 5, 15], 22))
