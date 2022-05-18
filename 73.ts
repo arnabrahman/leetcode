45681eceb02f155666d84f41717e920a5084fa46
arnabrahman <arnabrahman@hotmail.com>
2022-05-18 11:29:52 +0600

    Set Matrix Zeroes in constant space

--- 73.ts
+++ 73.ts
@@ -6,25 +6,32 @@
 function setZeroes(matrix: number[][]): void {
     const m = matrix.length
     const n = matrix[0].length
-    let row = Array(m).fill(0)
-    let col = Array(n).fill(0)
-
+    let row_zero = 1
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (matrix[i][j] != 0) continue;
-            row[i] = 1;
-            col[j] = 1
+            if (matrix[i][j] != 0) continue
+            i == 0 ? row_zero = 0 : matrix[i][0] = 0
+            matrix[0][j] = 0
         }
     }
-    
-    
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (row[i] == 1 || col[j] == 1) matrix[i][j] = 0
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                matrix[i][j] = 0
+            }
         }
     }
-    
+    if (matrix[0][0] == 0) {
+        for (let i = 1; i < m; i++) {
+            matrix[i][0] = 0
+        }
+    }
+    if (row_zero == 0) {
+        for (let i = 0; i < n; i++) {
+            matrix[0][i] = 0
+        }
+    }
 };
 
 
-console.log(setZeroes([[0,1,2,0],[3,4,5,2],[1,3,1,5]]));
+console.log(setZeroes([[1, 2, 0, 1, 5], [2, 3, 2, 3, 2], [0, 4, 1, 8, 7], [3, 3, 0, 1, 2]]));
