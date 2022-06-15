f81b829cdfaac297ad365352066405af56dbebae
arnabrahman <arnabrahman@hotmail.com>
2022-06-15 09:44:16 +0600

    LCS of Binary search tree

--- 235.ts
+++ 235.ts
@@ -1,15 +1,34 @@
+/**
+ *   LCS of Binary tree
+ *
+ */
+
+
+// function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+//     if (root == null) return null
+//     if ((root.left?.val == p.val && root.right?.val == q.val) || (root.left?.val == q.val && root.right?.val == p.val)) return root
+//     else if ((root.val == p.val || root.val == q.val) &&
+//         (hasDesc(root.left, p, q) || hasDesc(root.right, p, q))) return root
+//     else if (hasDesc(root.left, p, q) && hasDesc(root.right, p, q)) return root
+//     return lowestCommonAncestor(root.left, p, q) || lowestCommonAncestor(root.right, p, q)
+// }
+
+
+// function hasDesc(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): boolean {
+//     if (!root) return false
+//     else if (root.val == p.val || root.val == q.val) return true
+//     return hasDesc(root.left, p, q) || hasDesc(root.right, p, q)
+// }
+
+/**
+ *   LCS of Binary search tree
+ *
+ */
 function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
-    if (root == null) return null
-    if ((root.left?.val == p.val && root.right?.val == q.val) || (root.left?.val == q.val && root.right?.val == p.val)) return root
-    else if ((root.val == p.val || root.val == q.val) &&
-        (hasDesc(root.left, p, q) || hasDesc(root.right, p, q))) return root
-    else if (hasDesc(root.left, p, q) && hasDesc(root.right, p, q)) return root
-    return lowestCommonAncestor(root.left, p, q) || lowestCommonAncestor(root.right, p, q)
-}
-
-
-function hasDesc(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): boolean {
-    if (!root) return false
-    else if (root.val == p.val || root.val == q.val) return true
-    return hasDesc(root.left, p, q) || hasDesc(root.right, p, q)
-}
+    while (root) {
+        if (root.val < p.val && root.val < q.val) root = root.right
+        else if (root.val > p.val && root.val > q.val) root = root.left
+        else return root
+    }
+    return root
+};
