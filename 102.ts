51d25f54717d98726cfe658fd0579c467040aa2a
Arnab Rahman <arnabrahman@hotmail.com>
2022-06-15 21:29:16 +0600

    102. Binary Tree Level Order Traversal

--- 102.ts
+++ 102.ts
@@ -1,20 +1,20 @@
 function levelOrder(root: TreeNode | null): number[][] {
+    if (root == null) return []
     const res: number[][] = []
     const queue: TreeNode[] = []
-    let queueCursor = 0
     queue.push(root)
-    while (queueCursor != queue.length - 1) {
+    let queueCursor = 0
+    while (queueCursor < queue.length) {
         const until = queue.length
         const start = queueCursor
         const temp: number[] = []
         for (let i = start; i < until; i++) {
             temp.push(queue[i].val)
-            queueCursor++
             if (queue[i].left) queue.push(queue[i].left)
             if (queue[i].right) queue.push(queue[i].right)
+            queueCursor++
         }
         res.push(temp)
     }
     return res
 };
-
