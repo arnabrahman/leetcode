96a31b56c748800f7814f854f16337017caf7450
Arnab Rahman <arnabrahman@hotmail.com>
2022-06-17 14:40:45 +0600

    94. Inorder traversal iterative solution

--- 94.ts
+++ 94.ts
@@ -1,5 +1,18 @@
 function inorderTraversal(root: TreeNode | null): number[] {
-    return dfs(root, [])
+    let stack: TreeNode[] = root ? [root] : []
+    let curr = root
+    let result: number[] = []
+    while (stack.length > 0) {
+        while (curr && curr.left) {
+            curr = curr.left
+            stack.push(curr)
+        }
+        curr = stack.pop()
+        result.push(curr.val)
+        curr = curr.right
+        if (curr) stack.push(curr)
+    }
+    return result
 };
 
 function dfs(root: TreeNode | null, result: number[]) {
