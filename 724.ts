5c3e47377f9c2c2372b00ed575b2e249fe2c34b1
arnabrahman <arnabrahman@hotmail.com>
2022-06-27 08:55:16 +0600

    724. Find Pivot Index

---
+++ 724.ts
@@ -0,0 +1,12 @@
+function pivotIndex(nums: number[]): number {
+    const sum = nums.reduce((a, b) => a + b, 0)
+    let leftSum = 0
+    let righSum = 0
+    for (let i = 0; i < nums.length; i++) {
+        righSum = sum - leftSum - nums[i]
+        if (leftSum == righSum) return i
+        leftSum += nums[i]
+    }
+    return -1
+};
+console.log(pivotIndex([-2, 2, -1]))
