234aa6cc138817a8609b7e2598655b5d41b03734
Arnab Rahman <arnabrahman@hotmail.com>
2022-06-30 23:13:12 +0600

    876. Middle of the Linked List

--- 876.ts
+++ 876.ts
@@ -1,15 +1,28 @@
+// function middleNode(head: ListNode | null): ListNode | null {
+//     if (head.next == null) return head
+//     if (head.next.next == null) return head.next
+//     let cursor = head;
+//     let secondCursor = head.next.next
+//     let count = 3;
+//     while (secondCursor) {
+//         cursor = cursor.next
+//         if (secondCursor.next) count++
+//         if (secondCursor.next?.next) count++
+//         secondCursor = secondCursor.next?.next
+//     }
+//     if (count % 2 == 0) return cursor.next
+//     return cursor
+// };
+
+
 function middleNode(head: ListNode | null): ListNode | null {
-    if (head.next == null) return head
-    if (head.next.next == null) return head.next
-    let cursor = head;
-    let secondCursor = head.next.next
-    let count = 3;
-    while (secondCursor) {
-        cursor = cursor.next
-        if (secondCursor.next) count++
-        if (secondCursor.next?.next) count++
-        secondCursor = secondCursor.next?.next
+    if (head == null) return null
+    let slow = head
+    if (!slow.next) return slow
+    let fast = head.next?.next
+    while (fast?.next) {
+        fast = fast.next?.next
+        slow = slow.next
     }
-    if (count % 2 == 0) return cursor.next
-    return cursor
+    return slow.next
 };
