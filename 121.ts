b49e4f0d96528a975d9e963e7f6313d9fec638f6
Arnab Rahman <arnabrahman@hotmail.com>
2022-07-01 10:56:10 +0600

    121. Best Time to Buy and Sell Stock -> Redo again

--- 121.ts
+++ 121.ts
@@ -1,15 +1,26 @@
-function maxProfit(prices: number[]): number {
-    let bought = prices[0]
-    let maxProfit = 0
-    for (let i = 1; i < prices.length; i++) {
-        if (prices[i] < bought) {
-            bought = prices[i]
-            continue
-        } else {
-            maxProfit = maxProfit > (prices[i] - bought) ? maxProfit : (prices[i] - bought)
-        }
-    }
-    return maxProfit
-};
+// function maxProfit(prices: number[]): number {
+//     let bought = prices[0]
+//     let maxProfit = 0
+//     for (let i = 1; i < prices.length; i++) {
+//         if (prices[i] < bought) {
+//             bought = prices[i]
+//             continue
+//         } else {
+//             maxProfit = maxProfit > (prices[i] - bought) ? maxProfit : (prices[i] - bought)
+//         }
+//     }
+//     return maxProfit
+// };
 
-console.log(maxProfit([7, 6, 4, 3, 1]));
+
+function maxProfit(prices: number[]): number {
+    let max = 0;
+    let bought = prices[0];
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < bought) bought = prices[i]
+        max = prices[i] - bought > max ? prices[i] - bought : max
+    }
+    return max
+}
+
+console.log(maxProfit([7, 80, 4, 3, 1]));
