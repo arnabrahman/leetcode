08c7d44c24f816771203ddf0d8e57332d1998a79
Arnab Rahman <arnabrahman@hotmail.com>
2022-07-04 15:16:28 +0600

    98. Validate Binary Search Tree

--- 98.ts
+++ 98.ts
@@ -1,16 +1,14 @@
 function isValidBST(root: TreeNode | null): boolean {
-    return dfs(root, 0)
-    function dfs(root: TreeNode | null, val: number) {
-        if (root == null) return null
-        const left = dfs(root.left, val)
-        let mid = true
-        if (val) {
-            mid = root.val > val
-            val = root.val
-        }
-        const right = dfs(root.right, val)
-        return mid && left && right
-        // return (root.right ? root.val < root.right.val : true) && (root.left ? root.val > root.left.val : true) && left && right
+    let previousVal = null
+    let result = true
+    dfs(root)
+    return result
+    function dfs(root: TreeNode | null,) {
+        if (root == null || !result) return
+        dfs(root.left)
+        if (previousVal == null) previousVal = root.val
+        else if (root.val <= previousVal) result = false
+        else previousVal = root.val
+        dfs(root.right)
     }
-
 };
