2d08ea51e7e36179cd404c8a8e00619575336aef
Arnab Rahman <arnabrahman@hotmail.com>
2022-07-12 11:59:28 +0600

    199. Binary Tree Right Side View ->  Bad solution

--- 199.ts
+++ 199.ts
@@ -1,17 +1,44 @@
 function rightSideView(root: TreeNode | null): number[] {
-    const res: number[] = []
-    const stack: TreeNode[] = []
-    let cursor = 0
-    stack.push(root)
-    while (cursor < stack.length) {
-        const from = cursor
-        const until = stack.length
-        res.push(stack[cursor].val)
-        for (let i = from; i <= until; i++) {
-            if (stack[i].left) stack.push(stack[i].left)
-            if (stack[i].right) stack.push(stack[i].right)
-            cursor++
-        }
+    if (root == null) return []
+    enum child {
+        LEFT = 'l',
+        RIGHT = 'r',
+        ROOT = 'root'
     }
-    return res
+    const children: Array<child> = [child.ROOT]
+    const queue: Array<TreeNode> = []
+    const result: number[] = []
+    let queueCursor = 0
+    queue.push(root)
+    while (queueCursor < queue.length) {
+        const until = queue.length
+        const start = queueCursor
+        for (let i = start; i < until; i++) {
+            const hasChildren = children.length > 1
+            if (queue[i].right) {
+                queue.push(queue[i].right)
+                if (!hasChildren) children.push(child.RIGHT)
+                else children.push(children[i])
+            }
+            if (queue[i].left) {
+                queue.push(queue[i].left)
+                if (!hasChildren) children.push(child.LEFT)
+                else children.push(children[i])
+            }
+            queueCursor++
+        }
+        let firstLeft: number = 0
+        for (let i = start; i < queueCursor; i++) {
+            if (children[i] == child.ROOT) result.push(queue[i].val)
+            else if (children[i] == child.RIGHT) {
+                result.push(queue[i].val)
+                firstLeft = 0
+                break
+            }
+            else if (firstLeft == 0) firstLeft = queue[i].val
+        }
+        if (firstLeft != 0) result.push(firstLeft)
+    }
+    return result
 };
+console.log(rightSideView([1, 2, 3, null, 5, 5]));
